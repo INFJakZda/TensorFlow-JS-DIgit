46be892311aea9e9b3d677ad3d484b84e27f460d
INFJakZda <zdanowski78@gmail.com>
2019-05-27 22:23:01 +0200

    Add tensorflow model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as tf from '@tensorflow/tfjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tensorApp';
+
+  model: tf.Model;
+  predictions: any;
+
+  async loadModel() {
+    this.model = await tf.loadModel('/assets/model.json');
+  }
+
+  async predict(imageData: ImageData) {
+
+    await tf.tidy(() => {
+  
+      // Convert the canvas pixels to a Tensor of the matching shape
+      let img = tf.fromPixels(imageData, 1);
+      img = img.reshape([28, 28, 1]);
+      img = tf.cast(img, 'float32');
+  
+      // Make and format the predications
+      const output = this.model.predict(img) as any;
+  
+      // Save predictions on the component
+      this.predictions = Array.from(output.dataSync()); 
+    });
+  
+  }
 }
